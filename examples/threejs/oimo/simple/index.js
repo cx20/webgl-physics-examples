3ec1d3e4cfddf2a2737d251bc362b4ebd01968a0
cx20 <y-shimano@hondarer-soft.com>
2019-05-21 03:57:32 +0900

    Added three.js + Oimo.js example

---
+++ examples/threejs/oimo/simple/index.js
@@ -0,0 +1,96 @@
+let container;
+let camera, scene, renderer;
+let controls;
+let meshGround;
+let meshCube;
+let world;
+let body;
+
+function initOimo() {
+    world = new OIMO.World({ 
+        timestep: 1/30, 
+        iterations: 8, 
+        broadphase: 2, // 1 brute force, 2 sweep and prune, 3 volume tree
+        worldscale: 1, // scale full world 
+        random: true,  // randomize sample
+        info: false,   // calculate statistic or not
+        gravity: [0,-9.8,0] 
+    });
+
+    let groundBody = world.add({
+        type: "box",
+        size: [200, 2, 200],
+        pos: [0, -10, 0],
+        rot: [0, 0, 0],
+        move: false,
+        density: 1,
+        friction: 0.5,
+        restitution: 0.1,
+    });
+    body = world.add({
+        type: "box",
+        size: [50, 50, 50],
+        pos: [0, 100, 0],
+        rot: [10, 0, 10],
+        move: true,
+        density: 1,
+        friction: 0.5,
+        restitution: 0.2
+    });
+}
+
+function initThree() {
+    container = document.getElementById('container');
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.y = 50;
+    camera.position.z = 200;
+    scene = new THREE.Scene();
+
+    let loader = new THREE.TextureLoader();
+    let texture = loader.load('../../../../../assets/textures/frog.jpg');  // frog.jpg
+
+    let material = new THREE.MeshBasicMaterial({map: texture});
+    let geometryGround = new THREE.BoxGeometry(200, 2, 200);
+    meshGround = new THREE.Mesh(geometryGround, material);
+    meshGround.position.y = -10;
+    scene.add(meshGround);
+
+    let geometryCube = new THREE.BoxGeometry(50, 50, 50);
+    meshCube = new THREE.Mesh(geometryCube, material);
+    scene.add(meshCube);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xffffff);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    controls = new THREE.OrbitControls(camera);
+    controls.autoRotate = true;
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    updatePhysics();
+    controls.update();
+    render();
+}
+
+function updatePhysics() {
+    world.step();
+
+    meshCube.position.x = body.position.x;
+    meshCube.position.y = body.position.y;
+    meshCube.position.z = body.position.z;
+    meshCube.quaternion.x = body.quaternion.x;
+    meshCube.quaternion.y = body.quaternion.y;
+    meshCube.quaternion.z = body.quaternion.z;
+    meshCube.quaternion.w = body.quaternion.w;
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+initOimo();
+initThree();
+animate();
