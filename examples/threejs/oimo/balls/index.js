5e3c8c4c26e263b7da751520dc0d6bd2d6d11837
cx20 <y-shimano@hondarer-soft.com>
2019-05-26 20:39:56 +0900

    Added three.js + Oimo.js example

---
+++ examples/threejs/oimo/balls/index.js
@@ -0,0 +1,215 @@
+﻿// three var
+let camera, scene, light, renderer, container, content;
+let controls;
+
+let meshs = [];
+let grounds = [];
+let matSphere, matGround, matGroundTrans;
+let matSpheres = [];
+let buffgeoSphere, buffgeoBox;
+const ToRad = Math.PI / 180;
+const ToDeg = 180 / Math.PI;
+
+const dataSet = [
+    {imageFile:"../../../../assets/textures/Basketball.jpg", scale:1.0}, // Basketball.jpg
+    {imageFile:"../../../../assets/textures/BeachBall.jpg",  scale:0.9}, // BeachBall.jpg
+    {imageFile:"../../../../assets/textures/Football.jpg",   scale:1.0}, // Football.jpg
+    {imageFile:"../../../../assets/textures/Softball.jpg",   scale:0.3}, // Softball.jpg
+    {imageFile:"../../../../assets/textures/TennisBall.jpg", scale:0.3}, // TennisBall.jpg
+];
+let textures = [];
+
+//oimo var
+let world = new OIMO.World({ 
+    timestep: 1/30, 
+    iterations: 8, 
+    broadphase: 2, // 1 brute force, 2 sweep and prune, 3 volume tree
+    worldscale: 1, // scale full world 
+    random: true,  // randomize sample
+    info: false,   // calculate statistic or not
+    gravity: [0, -9.8, 0] 
+});
+let bodys = [];
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 1000);
+    camera.position.set(18, 20, 30);
+
+    scene = new THREE.Scene();
+
+    content = new THREE.Object3D();
+    scene.add(content);
+
+    scene.add(new THREE.AmbientLight(0x3D4143));
+
+    light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(30, 100, 50);
+    light.target.position.set(0, 0, 0);
+    light.castShadow = true;
+    light.shadow.camera = new THREE.OrthographicCamera(-30, 30, 30, -30, 0.1, 1000);
+    scene.add(light);
+
+    buffgeoSphere = new THREE.BufferGeometry();
+    buffgeoSphere.fromGeometry(new THREE.SphereGeometry(1, 20, 10));
+
+    buffgeoBox = new THREE.BufferGeometry();
+    buffgeoBox.fromGeometry(new THREE.BoxGeometry(1, 1, 1));
+
+    let loader = new THREE.TextureLoader();
+
+    for (let i = 0; i < dataSet.length; i++) {
+        let imageFile = dataSet[i].imageFile;
+        textures[i] = loader.load(imageFile);
+        matSpheres[i] = new THREE.MeshLambertMaterial({
+            map: textures[i],
+            name: 'sph' + i
+        });
+    }
+    matGround = new THREE.MeshLambertMaterial({
+        color: 0x3D4143,
+        transparent: false
+    });
+    matGroundTrans = new THREE.MeshLambertMaterial({
+        color: 0x3D4143,
+        transparent: true,
+        opacity: 0.6
+    });
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+
+    container = document.getElementById("container");
+    container.appendChild(renderer.domElement);
+
+    controls = new THREE.OrbitControls(camera);
+    controls.autoRotate = true;
+
+    initOimoPhysics();
+}
+
+function addStaticBox(size, position, rotation, spec) {
+    let mesh;
+    if (spec) mesh = new THREE.Mesh(buffgeoBox, matGroundTrans);
+    else mesh = new THREE.Mesh(buffgeoBox, matGround);
+    mesh.scale.set(size[0], size[1], size[2]);
+    mesh.position.set(position[0], position[1], position[2]);
+    mesh.rotation.set(rotation[0] * ToRad, rotation[1] * ToRad, rotation[2] * ToRad);
+    if (!grounds.length) content.add(mesh);
+    else scene.add(mesh);
+    grounds.push(mesh);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+}
+
+function initOimoPhysics() {
+    let max = 200;
+    let groundBody = world.add({
+        type: "box",
+        size: [40, 4, 40],
+        pos: [0, -2, 0],
+        rot: [0, 0, 0],
+        move: false,
+        density: 1,
+        friction: 0.6,
+        restitution: 0.5,
+    });
+    addStaticBox([40, 4, 40], [0, -2, 0], [0, 0, 0]);
+
+    let boxDataSet = [
+        { size:[10, 10,  1], pos:[ 0, 5,-5], rot:[0,0,0] },
+        { size:[10, 10,  1], pos:[ 0, 5, 5], rot:[0,0,0] },
+        { size:[ 1, 10, 10], pos:[-5, 5, 0], rot:[0,0,0] },
+        { size:[ 1, 10, 10], pos:[ 5, 5, 0], rot:[0,0,0] } 
+    ];
+
+    let surfaces = [];
+    for (let i = 0; i < boxDataSet.length; i++) {
+        let size = boxDataSet[i].size
+        let pos = boxDataSet[i].pos;
+        let rot = boxDataSet[i].rot;
+        let surfaceBody = world.add({
+            type: "box",
+            size: [size[0]/2, size[1]/2, size[2]/2],
+            pos: [pos[0], pos[1], pos[2]],
+            rot: [0, 0, 0],
+            move: false,
+            density: 1,
+            friction: 0.6,
+            restitution: 0.5,
+        });
+
+        addStaticBox(size, pos, rot, true);
+    }
+
+    let i = max;
+
+    while (i--) {
+        let x = -5 + Math.random() * 10;
+        let y = 20 + Math.random() * 10;
+        let z = -5 + Math.random() * 10;
+        let w = 2 + Math.random() * 1;
+        let h = 1 + Math.random() * 1;
+        let d = 1 + Math.random() * 1;
+
+        let pos = Math.floor(Math.random() * dataSet.length);
+        let scale = dataSet[pos].scale;
+        w *= scale;
+
+        let ballBody = world.add({
+            type: "sphere",
+            size: [w/2],
+            pos: [x, y, z],
+            rot: [0, 0, 0],
+            move: true,
+            density: 1,
+            friction: 0.4,
+            restitution: 0.6,
+        });
+        bodys[i] = ballBody;
+
+        meshs[i] = new THREE.Mesh(buffgeoSphere, matSpheres[pos]);
+        meshs[i].scale.set(w * 0.5, w * 0.5, w * 0.5);
+
+        meshs[i].castShadow = true;
+        meshs[i].receiveShadow = true;
+
+        scene.add(meshs[i]);
+    }
+}
+
+function updateOimoPhysics() {
+    world.step(1/30);
+    let i = bodys.length;
+
+    while (i--) {
+        let body = bodys[i];
+        let mesh = meshs[i];
+
+        mesh.position.x = body.position.x;
+        mesh.position.y = body.position.y;
+        mesh.position.z = body.position.z;
+        mesh.quaternion.x = body.quaternion.x;
+        mesh.quaternion.y = body.quaternion.y;
+        mesh.quaternion.z = body.quaternion.z;
+        mesh.quaternion.w = body.quaternion.w;
+        
+        if (mesh.position.y < -10) {
+            let x = -5 + Math.random() * 10;
+            let y = 20 + Math.random() * 10;
+            let z = -5 + Math.random() * 10;
+            body.resetPosition(x, y, z);
+        }
+    }
+}
+
+function loop() {
+    updateOimoPhysics();
+    renderer.render(scene, camera);
+    controls.update();
+    requestAnimationFrame(loop);
+}
+
+init();
+loop();
