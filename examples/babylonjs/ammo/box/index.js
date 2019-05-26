b87f222995bda0e876f57f2ee7ca8551fb1c2573
cx20 <y-shimano@hondarer-soft.com>
2019-05-26 11:35:39 +0900

    Added Babylon.js + ammo.js example

---
+++ examples/babylonjs/ammo/box/index.js
@@ -0,0 +1,151 @@
+﻿// ‥‥‥‥‥‥‥‥‥‥‥‥‥□□□
+// ‥‥‥‥‥‥〓〓〓〓〓‥‥□□□
+// ‥‥‥‥‥〓〓〓〓〓〓〓〓〓□□
+// ‥‥‥‥‥■■■□□■□‥■■■
+// ‥‥‥‥■□■□□□■□□■■■
+// ‥‥‥‥■□■■□□□■□□□■
+// ‥‥‥‥■■□□□□■■■■■‥
+// ‥‥‥‥‥‥□□□□□□□■‥‥
+// ‥‥■■■■■〓■■■〓■‥‥‥
+// ‥■■■■■■■〓■■■〓‥‥■
+// □□■■■■■■〓〓〓〓〓‥‥■
+// □□□‥〓〓■〓〓□〓〓□〓■■
+// ‥□‥■〓〓〓〓〓〓〓〓〓〓■■
+// ‥‥■■■〓〓〓〓〓〓〓〓〓■■
+// ‥■■■〓〓〓〓〓〓〓‥‥‥‥‥
+// ‥■‥‥〓〓〓〓‥‥‥‥‥‥‥‥
+let dataSet = [
+    "無","無","無","無","無","無","無","無","無","無","無","無","無","肌","肌","肌",
+    "無","無","無","無","無","無","赤","赤","赤","赤","赤","無","無","肌","肌","肌",
+    "無","無","無","無","無","赤","赤","赤","赤","赤","赤","赤","赤","赤","肌","肌",
+    "無","無","無","無","無","茶","茶","茶","肌","肌","茶","肌","無","赤","赤","赤",
+    "無","無","無","無","茶","肌","茶","肌","肌","肌","茶","肌","肌","赤","赤","赤",
+    "無","無","無","無","茶","肌","茶","茶","肌","肌","肌","茶","肌","肌","肌","赤",
+    "無","無","無","無","茶","茶","肌","肌","肌","肌","茶","茶","茶","茶","赤","無",
+    "無","無","無","無","無","無","肌","肌","肌","肌","肌","肌","肌","赤","無","無",
+    "無","無","赤","赤","赤","赤","赤","青","赤","赤","赤","青","赤","無","無","無",
+    "無","赤","赤","赤","赤","赤","赤","赤","青","赤","赤","赤","青","無","無","茶",
+    "肌","肌","赤","赤","赤","赤","赤","赤","青","青","青","青","青","無","無","茶",
+    "肌","肌","肌","無","青","青","赤","青","青","黄","青","青","黄","青","茶","茶",
+    "無","肌","無","茶","青","青","青","青","青","青","青","青","青","青","茶","茶",
+    "無","無","茶","茶","茶","青","青","青","青","青","青","青","青","青","茶","茶",
+    "無","茶","茶","茶","青","青","青","青","青","青","青","無","無","無","無","無",
+    "無","茶","無","無","青","青","青","青","無","無","無","無","無","無","無","無"
+];
+
+function getRgbColor( c )
+{
+    let colorHash = {
+        "無":[0xDC/0xFF, 0xAA/0xFF, 0x6B/0xFF],
+        "白":[0xff/0xFF, 0xff/0xFF, 0xff/0xFF],
+        "肌":[0xff/0xFF, 0xcc/0xFF, 0xcc/0xFF],
+        "茶":[0x80/0xFF, 0x00/0xFF, 0x00/0xFF],
+        "赤":[0xff/0xFF, 0x00/0xFF, 0x00/0xFF],
+        "黄":[0xff/0xFF, 0xff/0xFF, 0x00/0xFF],
+        "緑":[0x00/0xFF, 0xff/0xFF, 0x00/0xFF],
+        "水":[0x00/0xFF, 0xff/0xFF, 0xff/0xFF],
+        "青":[0x00/0xFF, 0x00/0xFF, 0xff/0xFF],
+        "紫":[0x80/0xFF, 0x00/0xFF, 0x80/0xFF]
+    };
+    return colorHash[c];
+}
+
+let engine;
+let scene;
+let canvas;
+
+document.addEventListener("DOMContentLoaded", function () {
+    onload();
+}, false);
+
+window.addEventListener("resize", function () {
+    if (engine) {
+        engine.resize();
+    }
+},false);
+
+let onload = function () {
+    canvas = document.querySelector("#c");
+    engine = new BABYLON.Engine(canvas, true);
+
+    createScene();
+
+    engine.runRenderLoop(function () {
+        scene.render();
+        scene.activeCamera.alpha += 0.01;
+    });
+};
+
+let createScene = function() {
+
+    scene = new BABYLON.Scene(engine);
+    scene.enablePhysics(new BABYLON.Vector3(0, -9.8, 0), new BABYLON.AmmoJSPlugin());
+    scene.getPhysicsEngine().setTimeStep(1 / 10);
+
+    let camera = new BABYLON.ArcRotateCamera("Camera", -2.2, 1.0, 500, BABYLON.Vector3.Zero(), scene);
+    camera.attachControl(canvas);
+    new BABYLON.HemisphericLight("hemi", new BABYLON.Vector3(0, 1, 0), scene);
+    new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(0.0, -1.0, 0.5), scene);
+
+    let mat = new BABYLON.StandardMaterial("ground", scene);
+    let t = new BABYLON.Texture("../../../../assets/textures/grass.jpg", scene); // grass.jpg
+
+    t.uScale = t.vScale = 2;
+    mat.diffuseTexture = t;
+    mat.specularColor = BABYLON.Color3.Black();
+    let g = BABYLON.Mesh.CreateBox("ground", 400, scene);
+    g.position.y = -20;
+    g.scaling.y = 0.01;
+    g.material = mat;
+    g.physicsImpostor = new BABYLON.PhysicsImpostor(g, BABYLON.PhysicsImpostor.BoxImpostor, {
+        move: false,
+        mass: 0,
+        friction: 1.0,
+        restitution: 1.0
+    }, scene);
+
+    // Get a random number between two limits
+    let randomNumber = function(min, max) {
+        if (min == max) {
+            return (min);
+        }
+        let random = Math.random();
+        return ((random * (max - min)) + min);
+    };
+
+    let objects = [];
+    let getPosition = function(y) {
+        return new BABYLON.Vector3(randomNumber(-25, 25), randomNumber(0, 100) + y, randomNumber(-25, 25));
+    };
+
+    const BOX_SIZE = 15;
+    var i = 0;
+    for (var y = 0; y < 16; y++) {
+        for (var x = 0; x < 16; x++) {
+            i = x + (15 - y) * 16;
+            var s = BABYLON.Mesh.CreateBox("Box" + String(i), BOX_SIZE, scene);
+            var x1 = -130 + x * BOX_SIZE * 1.2 + Math.random();
+            var y1 = 30 + y * BOX_SIZE * 1.2;
+            var z1 = 1.0 * Math.random();
+            s.position = new BABYLON.Vector3(x1, y1, z1);
+            var matCube = new BABYLON.StandardMaterial("ball", scene);
+            var rgbColor = getRgbColor(dataSet[i]);
+            matCube.diffuseColor = new BABYLON.Color3(rgbColor[0], rgbColor[1], rgbColor[2]);
+            s.material = matCube;
+            s.physicsImpostor = new BABYLON.PhysicsImpostor(s, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, friction: 1.0, restitution: 0.0}, scene);
+
+            // SAVE OBJECT
+            objects.push(s);
+        }
+    }
+    scene.registerBeforeRender(function() {
+        objects.forEach(function(obj) {
+            if (obj.position.y < -100) {
+                obj.position = getPosition(200);
+                obj.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0,0,0));
+            }
+        });
+    });
+/*    
+*/
+};
