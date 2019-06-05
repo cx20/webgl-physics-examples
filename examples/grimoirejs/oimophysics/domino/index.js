ac8f224b02a3a3f6886bea8887eef41a0eca9034
cx20 <y-shimano@hondarer-soft.com>
2019-06-06 07:56:25 +0900

    Added Grimoire.js + OimoPhysics.js example

---
+++ examples/grimoirejs/oimophysics/domino/index.js
@@ -0,0 +1,130 @@
+﻿// ‥‥‥‥‥‥‥‥‥‥‥‥‥□□□
+// ‥‥‥‥‥‥〓〓〓〓〓‥‥□□□
+// ‥‥‥‥‥〓〓〓〓〓〓〓〓〓□□
+// ‥‥‥‥‥■■■□□■□‥■■■
+// ‥‥‥‥■□■□□□■□□■■■
+// ‥‥‥‥■□■■□□□■□□□■
+// ‥‥‥‥■■□□□□■■■■■‥
+// ‥‥‥‥‥‥□□□□□□□■‥‥
+// ‥‥■■■■■〓■■■〓■‥‥‥
+// ‥■■■■■■■〓■■■〓‥‥■
+// □□■■■■■■〓〓〓〓〓‥‥■
+// □□□‥〓〓■〓〓□〓〓□〓■■
+// ‥□‥■〓〓〓〓〓〓〓〓〓〓■■
+// ‥‥■■■〓〓〓〓〓〓〓〓〓■■
+// ‥■■■〓〓〓〓〓〓〓‥‥‥‥‥
+// ‥■‥‥〓〓〓〓‥‥‥‥‥‥‥‥
+var dataSet = [
+    "BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BK","BG","BG","BG",
+    "BK","BK","BK","BK","BK","BK","RD","RD","RD","RD","RD","BK","BK","BG","BG","BG",
+    "BK","BK","BK","BK","BK","RD","RD","RD","RD","RD","RD","RD","RD","RD","BG","BG",
+    "BK","BK","BK","BK","BK","BR","BR","BR","BG","BG","BR","BG","BK","RD","RD","RD",
+    "BK","BK","BK","BK","BR","BG","BR","BG","BG","BG","BR","BG","BG","RD","RD","RD",
+    "BK","BK","BK","BK","BR","BG","BR","BR","BG","BG","BG","BR","BG","BG","BG","RD",
+    "BK","BK","BK","BK","BR","BR","BG","BG","BG","BG","BR","BR","BR","BR","RD","BK",
+    "BK","BK","BK","BK","BK","BK","BG","BG","BG","BG","BG","BG","BG","RD","BK","BK",
+    "BK","BK","RD","RD","RD","RD","RD","BL","RD","RD","RD","BL","RD","BK","BK","BK",
+    "BK","RD","RD","RD","RD","RD","RD","RD","BL","RD","RD","RD","BL","BK","BK","BR",
+    "BG","BG","RD","RD","RD","RD","RD","RD","BL","BL","BL","BL","BL","BK","BK","BR",
+    "BG","BG","BG","BK","BL","BL","RD","BL","BL","YL","BL","BL","YL","BL","BR","BR",
+    "BK","BG","BK","BR","BL","BL","BL","BL","BL","BL","BL","BL","BL","BL","BR","BR",
+    "BK","BK","BR","BR","BR","BL","BL","BL","BL","BL","BL","BL","BL","BL","BR","BR",
+    "BK","BR","BR","BR","BL","BL","BL","BL","BL","BL","BL","BK","BK","BK","BK","BK",
+    "BK","BR","BK","BK","BL","BL","BL","BL","BK","BK","BK","BK","BK","BK","BK","BK"
+];
+
+function getRgbColor(colorType)
+{
+    var colorHash = {
+        "BK":"#dcaa6b", // black
+        "WH":"#FFFFFF", // white
+        "BG":"#FFCCCC", // beige
+        "BR":"#800000", // brown
+        "RD":"#FF0000", // red
+        "YL":"#FFFF00", // yellow
+        "GN":"#00FF00", // green
+        "WT":"#00FFFF", // water
+        "BL":"#0000FF", // blue
+        "PR":"#800080"  // purple
+    };
+    return colorHash[colorType];
+}
+
+const Quaternion = gr.lib.math.Quaternion;
+gr(function() {
+    const scene = gr("#main")("scene").single();
+    for ( var x = 0; x < 16; x++ ) {
+        for ( var z = 0; z < 16; z ++ ) {
+            i = x + (z) * 16;
+            y = -3.5;
+            const n = scene.addChildByName("rigid-cube", {
+                albedo: getRgbColor(dataSet[i]),
+                scale: [0.2 * 0.5, 1.2 * 0.5, 1.0 * 0.5],
+                position: [x*1.1-8, y, z*1.1-8]
+            });
+        }
+    }
+    for ( i = 0; i < 16; i++ ) {
+        x = -0.4;
+        y = -2;
+        z = i;
+        const n = scene.addChildByName("rigid-cube", {
+            albedo: "#ff0000",
+            scale: [0.5, 0.5, 0.5],
+            position: [x*1.1-8, y, z*1.1-8]
+        });
+    }
+});
+gr.register(() => {
+    gr.registerComponent("OimoScene", {
+        attributes: {
+        },
+        $awake: function() {
+            this.world = new OIMO.World();
+            this.world.gravity = new OIMO.Vec3(0, -9.80665, 0);
+        },
+        $update: function() {
+            this.world.step(1/30);
+        }
+    });
+    gr.overrideDeclaration("scene", ["OimoScene"]);
+    gr.registerComponent("Rigid", {
+        attributes: {
+            shape: {
+                default: "box",
+                converter: "String"
+            },
+            move: {
+                converter: "Boolean",
+                default: true
+            }
+        },
+        $mount: function() {
+            this.__bindAttributes();
+            this.transform = this.node.getComponent("Transform");
+            const p = this.transform.position;
+            const r = this.transform.rotation;
+            const s = this.transform.scale;
+            const oimoScene = this.node.getComponentInAncestor("OimoScene");
+            const shapec = new OIMO.ShapeConfig();
+            shapec.geometry = new OIMO.BoxGeometry(new OIMO.Vec3(s.X, s.Y, s.Z));
+            const bodyc = new OIMO.RigidBodyConfig();
+            bodyc.type = this.move ? OIMO.RigidBodyType.DYNAMIC : OIMO.RigidBodyType.STATIC;
+            bodyc.position = new OIMO.Vec3(p.X, p.Y, p.Z);
+            this.body = new OIMO.RigidBody(bodyc);
+            this.body.setRotationXyz(new OIMO.Vec3(r.X, r.Y, r.Z));
+            this.body.addShape(new OIMO.Shape(shapec));
+            oimoScene.world.addRigidBody(this.body);
+        },
+        $update: function() {
+            const p = this.body.getPosition();
+            this.transform.setAttribute("position", [p.x, p.y, p.z]);
+            const r = this.body.getOrientation();
+            this.transform.setAttribute("rotation", new Quaternion([r.x, r.y, r.z, r.w]));
+        }
+    });
+    gr.registerNode("rigid-cube", ["Rigid"], {
+        geometry: "cube",
+        scale: 0.5
+    }, "mesh");
+});
