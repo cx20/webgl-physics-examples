83d93010429117246bc2ad21905b45ef6cc01369
cx20 <y-shimano@hondarer-soft.com>
2022-04-04 23:47:51 +0900

    Update Rhodonite v0.5.2 to v0.5.6

--- examples/rhodonite/oimo/minimum/index.js
+++ examples/rhodonite/oimo/minimum/index.js
@@ -35,119 +35,10 @@ function initOimo() {
     });
 }
 
-function generateEntity() {
-    const repo = Rn.EntityRepository.getInstance();
-    const entity = repo.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.MeshComponent, Rn.MeshRendererComponent]);
-    return entity;
-}
-
-function readyBasicVerticesData() {
-
-    // Cube data
-    //             1.0 y 
-    //              ^  -1.0 
-    //              | / z
-    //              |/       x
-    // -1.0 -----------------> +1.0
-    //            / |
-    //      +1.0 /  |
-    //           -1.0
-    // 
-    //         [7]------[6]
-    //        / |      / |
-    //      [3]------[2] |
-    //       |  |     |  |
-    //       | [4]----|-[5]
-    //       |/       |/
-    //      [0]------[1]
-    //
-    const positions = new Float32Array([ 
-        // Front face
-        -0.5, -0.5,  0.5, // v0
-         0.5, -0.5,  0.5, // v1
-         0.5,  0.5,  0.5, // v2
-        -0.5,  0.5,  0.5, // v3
-        // Back face
-        -0.5, -0.5, -0.5, // v4
-         0.5, -0.5, -0.5, // v5
-         0.5,  0.5, -0.5, // v6
-        -0.5,  0.5, -0.5, // v7
-        // Top face
-         0.5,  0.5,  0.5, // v2
-        -0.5,  0.5,  0.5, // v3
-        -0.5,  0.5, -0.5, // v7
-         0.5,  0.5, -0.5, // v6
-        // Bottom face
-        -0.5, -0.5,  0.5, // v0
-         0.5, -0.5,  0.5, // v1
-         0.5, -0.5, -0.5, // v5
-        -0.5, -0.5, -0.5, // v4
-         // Right face
-         0.5, -0.5,  0.5, // v1
-         0.5,  0.5,  0.5, // v2
-         0.5,  0.5, -0.5, // v6
-         0.5, -0.5, -0.5, // v5
-         // Left face
-        -0.5, -0.5,  0.5, // v0
-        -0.5,  0.5,  0.5, // v3
-        -0.5,  0.5, -0.5, // v7
-        -0.5, -0.5, -0.5  // v4
-    ]);
-    
-    const texcoords = new Float32Array([ 
-        // Front face
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        
-        // Back face
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        0.0, 0.0,
-        
-        // Top face
-        0.0, 1.0,
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        
-        // Bottom face
-        1.0, 1.0,
-        0.0, 1.0,
-        0.0, 0.0,
-        1.0, 0.0,
-        
-        // Right face
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        0.0, 0.0,
-        
-        // Left face
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0
-    ]);
-
-    const indices = new Uint32Array([
-         0,  1,  2,    0,  2 , 3,  // Front face
-         4,  5,  6,    4,  6 , 7,  // Back face
-         8,  9, 10,    8, 10, 11,  // Top face
-        12, 13, 14,   12, 14, 15,  // Bottom face
-        16, 17, 18,   16, 18, 19,  // Right face
-        20, 21, 22,   20, 22, 23   // Left face
-    ]);
-        
-    const primitive = Rn.Primitive.createPrimitive({
-        indices: indices,
-        attributeCompositionTypes: [Rn.CompositionType.Vec3, Rn.CompositionType.Vec2],
-        attributeSemantics: [Rn.VertexAttribute.Position, Rn.VertexAttribute.Texcoord0],
-        attributes: [positions, texcoords],
-        primitiveMode: Rn.PrimitiveMode.Triangles
-    });
+function readyCubeVerticesData() {
+    let modelMaterial = Rn.MaterialHelper.createClassicUberMaterial();
+    const primitive = new Rn.Cube();
+    primitive.generate({ widthVector: Rn.Vector3.fromCopy3(0.5, 0.5, 0.5) });
 
     const texture = new Rn.Texture();
     texture.generateTextureFromUri('../../../../assets/textures/frog.jpg');
@@ -180,70 +71,43 @@ const load = async function () {
         gl.viewport(0, 0, c.width, c.height);
     }
     
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    const primitive = readyBasicVerticesData();
-
-    Rn.MeshRendererComponent.manualTransparentSids = [];
+    const primitive = readyCubeVerticesData();
 
     const entities = [];
     // Ground
     const mesh1 = new Rn.Mesh();
     mesh1.addPrimitive(primitive);
-    const entity1 = generateEntity();
+    const entity1 = Rn.EntityHelper.createMeshEntity();
     entity1.tryToSetTag({tag:"type", value:"ground"});
 
     entities.push(entity1);
-    const meshComponent1 = entity1.getComponent(Rn.MeshComponent);
+    const meshComponent1 = entity1.getMesh();
 
     meshComponent1.setMesh(mesh1);
-    entity1.getTransform().scale = Rn.Vector3.fromCopyArray([200, 2, 200]);
-    entity1.getTransform().toUpdateAllTransform = false;
+    entity1.scale = Rn.Vector3.fromCopyArray([200, 2, 200]);
 
     // Cube
     const mesh2 = new Rn.Mesh();
     mesh2.addPrimitive(primitive);
-    const entity2 = generateEntity();
+    const entity2 = Rn.EntityHelper.createMeshEntity();
     entity2.tryToSetTag({tag:"type", value:"cube"});
 
     entities.push(entity2);
-    const meshComponent2 = entity2.getComponent(Rn.MeshComponent);
+    const meshComponent2 = entity2.getMesh();
 
     meshComponent2.setMesh(mesh2);
-    entity2.getTransform().scale = Rn.Vector3.fromCopyArray([50, 50, 50]);
-    entity2.getTransform().toUpdateAllTransform = false;
+    entity2.scale = Rn.Vector3.fromCopyArray([50, 50, 50]);
 
     const startTime = Date.now();
-    let p = null;
-    const rotationVec3 = Rn.MutableVector3.zero();
-    let count = 0
-
+   
     // camera
-    const cameraComponent = createCameraComponent();
+    const cameraEntity = Rn.EntityHelper.createCameraControllerEntity();
+    cameraEntity.translate = Rn.Vector3.fromCopyArray([0, 50, 200]);
+    const cameraComponent = cameraEntity.getCamera();
     cameraComponent.zNear = 0.1;
     cameraComponent.zFar = 1000;
     cameraComponent.setFovyAndChangeFocalLength(45);
     cameraComponent.aspect = window.innerWidth / window.innerHeight;
-    const cameraEntity = cameraComponent.entity;
-    cameraEntity.getTransform().translate = Rn.Vector3.fromCopyArray([0, 50, 200]);
-
-    // renderPass
-    const renderPass = new Rn.RenderPass();
-    renderPass.cameraComponent = cameraComponent;
-    renderPass.toClearColorBuffer = true;
-    renderPass.addEntities(entities);
-
-    // expression
-    const expression = new Rn.Expression();
-    expression.addRenderPasses([renderPass]);
-
-    function createCameraComponent() {
-        const entityRepository = Rn.EntityRepository.getInstance();
-        const cameraEntity = entityRepository.createEntity([Rn.TransformComponent, Rn.SceneGraphComponent, Rn.CameraComponent]);
-        const cameraComponent = cameraEntity.getComponent(Rn.CameraComponent);
-        return cameraComponent;
-    }
     
     function updatePhysics() {
         world.step();
@@ -252,8 +116,8 @@ const load = async function () {
 
         entities.forEach(function (entity) {
             if ( entity.getTagValue("type") == "cube" ) {
-                entity.getTransform().translate = Rn.Vector3.fromCopyArray([p.x, p.y, p.z]);
-                entity.getTransform().quaternion = Rn.Vector4.fromCopyArray4([q.x, q.y, q.z, q.w]);
+                entity.translate = Rn.Vector3.fromCopyArray([p.x, p.y, p.z]);
+                entity.quaternion = Rn.Vector4.fromCopyArray4([q.x, q.y, q.z, q.w]);
             }
         });
     }
@@ -262,9 +126,8 @@ const load = async function () {
         updatePhysics();
 
         gl.disable(gl.CULL_FACE); // TODO:
-        system.process([expression]);
+        system.processAuto();
 
-        count++;
         requestAnimationFrame(draw);
     }
 
