0ad1533323e83396a447ed91f9dbb92af7ee44a1
cx20 <y-shimano@hondarer-soft.com>
2023-05-04 11:05:25 +0900

    Improved Havok physics sample for Babylon.js

--- examples/babylonjs/havok/balls/index.js
+++ examples/babylonjs/havok/balls/index.js
@@ -138,13 +138,10 @@ const createScene = function() {
                 const body = obj.aggregate.body;
                 const pos = getPosition(100);
 
-                body.disablePreStep = true;
+                body.disablePreStep = false;
                 body.transformNode.position.set(pos.x, pos.y, pos.z);
                 body.setLinearVelocity(new BABYLON.Vector3(0,0,0));
                 body.setAngularVelocity(new BABYLON.Vector3(0,0,0));
-
-                body.disablePreStep = false;
-
             }
         });
         scene.activeCamera.alpha += Math.PI * 1.0 / 180.0 * scene.getAnimationRatio();
